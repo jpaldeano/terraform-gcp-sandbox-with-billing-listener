2b7b260c770921213bcc1b6439343b9a6fb72a11
João Aldeano <jotaldeano@gmail.com>
2021-01-01 20:09:57 +0000

    added function, bucket, service account and function IAM

--- project.tf
+++ project.tf
@@ -24,7 +24,7 @@ data "google_billing_account" "account" {
 resource "google_billing_budget" "budget" {
   provider = google-beta
   billing_account = data.google_billing_account.account.id
-  display_name = "Example Billing Budget"
+  display_name = var.budget_name
 
   budget_filter {
     projects = ["projects/${google_project.my_project.project_id}"]
@@ -47,3 +47,47 @@ resource "google_billing_budget" "budget" {
 
   depends_on = [google_pubsub_topic.my_topic]
 }
+
+resource "google_service_account" "service_account" {
+  account_id   = var.service_account
+  display_name = var.service_account_name
+  project = var.terraform_project_id
+}
+
+resource "google_storage_bucket" "bucket" {
+  name = var.bucket_name
+}
+
+/* 
+resource "google_storage_bucket_object" "archive" {
+  name   = var.function_code_filename
+  bucket = google_storage_bucket.bucket.name
+  source = var.function_code_source
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = var.function_name
+  description = var.function_description
+  runtime     = var.function_runtime
+
+  available_memory_mb   = var.function_available_memory_mb
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = var.function_trigger_http
+  timeout               = var.function_timeout
+  entry_point           = var.function_entry_point
+
+  // environment_variables = {
+    // MY_ENV_VAR = "my-env-var-value"
+  // }
+}
+
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = var.function_iam_role
+  member = "serviceAccount:${google_service_account.service_account}"
+}
+*/
--- variables.tf
+++ variables.tf
@@ -28,3 +28,42 @@ variable "project_id" {
 
 variable "org_id" {
 }
+
+variable "service_account_id" {
+}
+
+variable "service_account_name" {
+}
+
+variable "bucket_name" {
+}
+
+variable "function_code_filename" {
+}
+
+variable "function_code_source" {
+}
+
+variable "function_name" {
+}
+
+variable "function_description" {
+}
+
+variable "function_runtime" {
+}
+
+variable "function_available_memory_mb" {
+}
+
+variable "function_trigger_http" {
+}
+
+variable "function_timeout" {
+}
+
+variable "function_entry_point" {
+}
+
+variable "function_iam_role" {
+}
